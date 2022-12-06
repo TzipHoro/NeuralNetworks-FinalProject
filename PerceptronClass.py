216a507d54ae4761eb9605365c172ac66e94def7
elegantmoose <mkouremetis16@gmail.com>
2022-12-05 19:36:51 -0500

    type hinting and pythonisms

--- PerceptronClass.py
+++ PerceptronClass.py
@@ -9,56 +9,60 @@ import numpy as np
 
 
 class Perceptron:
-    def __init__(self, initial_weights, bias):
+    def __init__(self, initial_weights: list , bias: float):
         self.weights = initial_weights 
         self.bias = bias 
-        self.delta = 0 
+        self._delta = 0 
         self.delta_weights = [0] * len(self.weights)
         self.activity = 0 
-        self.activation = 0 
+        self._activation = 0 
         # creates a vector to know the number of inputs associated with the weights
         self.vector_length = len(self.weights)
+
+    @property
+    def activation(self):
+        return self._activation
+    
+    @activation.setter
+    def activation(self, value):
+        self._activation = value
+
+    @property
+    def delta(self):
+        return self._delta
+    
+    @delta.setter
+    def delta(self, value):
+        self._delta = value
         
-    def calc_activity(self, input_vector, bias_bool):
+    def calc_activity(self, input_vector: list, bias: bool):
         """ """
         input = input_vector
         A = 0
-        # for loop to calculate activity based on input/weight pairs
         for i in range(0, self.vector_length):
             A += input[i] * self.weights[i]
-        if bias_bool == 1:    
+        if bias == 1:    
             A += self.bias
         self.activity = A
         
-    def calc_activation(self, input_vector, bias_bool):
+    def calc_activation(self, input_vector: list, bias: bool):
         """ """
-        self.calc_activity(input_vector, bias_bool)
-        self.activation = 1.0/(1.0 + np.exp(-1*self.activity))
+        self.calc_activity(input_vector, bias)
+        self._activation = 1.0/(1.0 + np.exp(-1*self.activity))
     
-    def get_activation(self):
-        """ """
-        return(self.activation)
     
-    def set_delta(self, delta):
-        """ """
-        self.delta = delta
-        
-    def get_delta(self):
-        """ """
-        return(self.delta)
-    
-    def set_delta_weights(self, input_vector, eta):
+    def set_delta_weights(self, input_vector: list, eta: float):
         """ """
         input = input_vector
         # calcuates the change to the weights
         for i in range(0, self.vector_length):
-            self.delta_weights[i] = input[i] * eta * self.delta
+            self.delta_weights[i] = input[i] * eta * self._delta
         
     def update_weights(self):
         """ """
         for i in range(0, self.vector_length):
-            self.weights[i] = self.weights[i] + self.delta_weights[i]
+            self.weights[i] += self.delta_weights[i]
             
-    def update_bias(self, eta):
+    def update_bias(self, eta: float):
         """ """
-        self.bias = self.bias + eta * self.delta * 1
+        self.bias += eta * self.delta * 1
