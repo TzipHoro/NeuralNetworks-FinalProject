013da9a4aca99a298ea137e7e77e50bef04b1d73
elegantmoose <mkouremetis16@gmail.com>
2022-12-05 21:21:20 -0500

    adhoc changes

--- MultiPerceptron_Training.py
+++ MultiPerceptron_Training.py
@@ -15,14 +15,14 @@ np.set_printoptions(precision=4, suppress=True)
 def FFBP(H_list, O_list, input_vector, iterations, eta, bias_update,
     delta_j, delta_k, O_0, y, z, d):
     """Function to run the feedforward back propagation training method"""
-    for i in range(0,iterations):
+    for i in range(0, iterations):
         # FEEDFORWARD
-        for j in range(0,(len(H_list))):
+        for j in range(0, len(H_list)):
             H_list[j].calc_activity(input_vector, bias_update)
             H_list[j].calc_activation(input_vector, bias_update)
             y[j] = H_list[j].activation
             
-        for k in range(0,(len(O_list))):
+        for k in range(0, len(O_list)):
             O_list[k].calc_activity(y, bias_update)
             O_list[k].calc_activation(y, bias_update)
             z[k] = O_list[k].activation
@@ -35,16 +35,16 @@ def FFBP(H_list, O_list, input_vector, iterations, eta, bias_update,
             # sets the delta value 
             O_list[k].delta = delta_k[k]
             
-        for l in range(0,(len(H_list))):
+        for l in range(0, len(H_list)):
             # calculates the delta value for the hidden layer before updating output weights
             delta_j[l] = (1 - y[l]) * y[l] * (sum(delta_k) * O_0.weights[l])
             
-        for m in range(0,(len(O_list))):
+        for m in range(0, len(O_list)):
             O_list[k].set_delta_weights(y, eta)
             O_list[k].update_weights()
             O_list[k].update_bias(eta)
             
-        for n in range(0,(len(H_list))):
+        for n in range(0, len(H_list)):
             H_list[n].delta = delta_j[n]
             H_list[n].set_delta_weights(input_vector, eta)
             H_list[n].update_weights()
@@ -77,7 +77,7 @@ def main():
     delta_k = [0] * len(O_list)  # preallocates space for delta_k values
 
     # Online Training
-    for i in range(1,31):
+    for i in range(1, 31):
         # even training cycle
         if (i % 2) == 0:
             # variable update
